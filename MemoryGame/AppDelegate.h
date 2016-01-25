c31d79c6946513595b1fbb118ffc6ba16133905a
Steven yang <steven_yang9190@hotmail.com>
2016-01-25 14:28:26 -0800

    Initial Commit

---
+++ MemoryGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MemoryGame
+//
+//  Created by Steven Yang on 1/25/16.
+//  Copyright © 2016 Le Mont. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
