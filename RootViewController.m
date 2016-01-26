f8ec2fc30417dc6690fbd56076725e6ad491bafb
Steven yang <steven_yang9190@hotmail.com>
2016-01-25 16:34:20 -0800

    Intial Commit Fix yo

--- RootViewController.m
+++ RootViewController.m
@@ -9,20 +9,57 @@
 #import "RootViewController.h"
 
 @interface RootViewController ()
-
+@property (weak, nonatomic) IBOutlet UIView *instructionView;
+@property NSMutableArray *cards;
+@property NSMutableArray *cardsInPlay;
 @end
 
 @implementation RootViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.cards = [NSMutableArray arrayWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Assets" ofType:@"xcassets"]];
+    NSUserDefaults *userDefault = [NSUserDefaults standardUserDefaults];
+    if (![userDefault boolForKey:@"firstLaunch"]) {
+        [userDefault setBool:YES forKey:@"firstLaunch"];
+        self.instructionView.hidden = NO;
+        
+    }
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)loadCardsInPlay {
+    NSMutableArray *randomStorage = [NSMutableArray new];
+    for (int i = 0; i < 8; i++) {
+        int random = arc4random_uniform(self.cards.count);
+        if (![randomStorage containsObject:[NSNumber numberWithInt:random]])
+        {
+            [self.cardsInPlay addObject:[self.cards objectAtIndex:random]];
+            [self.cardsInPlay addObject:[self.cards objectAtIndex:random]];
+            [randomStorage addObject:[NSNumber numberWithInt:random]];
+        } else
+        {
+            i--;
+        }
+        
+    }
+    
 }
+
+
+-(void)shuffleCards
+{
+    
+}
+
+
+#pragma mark -actions
+
+- (IBAction)onDuelButtonTapped:(UIButton *)sender {
+    [sender.superview removeFromSuperview];
+}
+
+
 
 /*
 #pragma mark - Navigation
