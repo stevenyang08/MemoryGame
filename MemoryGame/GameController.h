0513a19b4309f5cf8d20247e704538b5c5d0d1c5
Steven yang <steven_yang9190@hotmail.com>
2016-01-25 16:31:21 -0800

    first commit

---
+++ MemoryGame/GameController.h
@@ -0,0 +1,13 @@
+//
+//  GameController.h
+//  MemoryGame
+//
+//  Created by Steven Yang on 1/25/16.
+//  Copyright © 2016 Le Mont. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameController : NSObject
+
+@end
---
+++ MemoryGame/GameController.m
@@ -0,0 +1,13 @@
+//
+//  GameController.m
+//  MemoryGame
+//
+//  Created by Steven Yang on 1/25/16.
+//  Copyright © 2016 Le Mont. All rights reserved.
+//
+
+#import "GameController.h"
+
+@implementation GameController
+
+@end
